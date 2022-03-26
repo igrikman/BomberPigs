e375bdecab4acfcd522dec9e2af0118d1536191c
DESKTOP-92KNQDD\lider <lider.x2011@yandex.ru>
2022-03-26 04:21:06 +0300

    Joystik&Button; Bomb;

--- Bomber/Assets/Scripts/Sc_Player.cs
+++ Bomber/Assets/Scripts/Sc_Player.cs
@@ -10,23 +10,40 @@ public class Sc_Player : MonoBehaviour
     [SerializeField] private float speed;
     private Vector2 direction;
     private Rigidbody2D rb;
+    [SerializeField] private GameObject bombPrefab;
+    private bool bombActive = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    public void SpawnBomd()
+    {
+        if (bombActive == false)
+        {
+            Instantiate(bombPrefab, gameObject.transform.position, new Quaternion());
+            bombActive = true;
+        }
+    }
     private void Update()
     {
         if (controlType == ControlType.PC)
         {
-            //direction.x = Input.GetAxisRaw("Horizontal");
-            //direction.y = Input.GetAxisRaw("Vertical");
             direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         }
-        else if(controlType == ControlType.Android)
+        if (controlType == ControlType.PC)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                SpawnBomd();
+            }
+        }
+        else if (controlType == ControlType.Android)
         {
             direction = new Vector2(joystick.Horizontal, joystick.Vertical);
         }
+
     }
     private void FixedUpdate()
     {
